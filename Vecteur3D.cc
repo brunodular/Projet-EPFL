5febae293b08061c3903ef24589b4282a3956d4f
HDHD2000 <47893466+HDHD2000@users.noreply.github.com>
2019-02-24 13:29:48 +0100

    Add files via upload

--- Vecteur3D.cc
+++ Vecteur3D.cc
@@ -1,92 +1,80 @@
 #include <iostream>
-#include "Vecteur3D.h"
 #include <cmath>
-using namespace std;
+#include "Vecteur3D.h"
+
+//--------------------------------------------------------------
+
+/*Fonction d'affichage utilisee dans les tests*/
 
 void Vecteur3D::affiche() const {
-  cout << x_ << ' ' << y_ << ' ' << z_;
-}
+	cout.setf(ios::showpoint);
+	std::cout << x << " " << y << " " << z << std::endl;
+};
 
-bool Vecteur3D::compare(Vecteur3D autre) const {
-  return x_ == autre.x() and y_ == autre.y() and z_ == autre.z();
-}
 
-void Vecteur3D::set(double x,double y,double z) {
-  x_ = x; y_ = y; z_ = z;
-}
+bool Vecteur3D::compare(Vecteur3D v) const  {
+	return (v.x==x) and (v.y==y) and (v.z==z);
+};
 
-Vecteur3D Vecteur3D::addition(Vecteur3D autre) const {
-  Vecteur3D resultat;
-  resultat.x(x_+autre.x());
-  resultat.y(y_+autre.y());
-  resultat.z(z_+autre.z());
-  return resultat;
-}
+//---------------------------------------------------------------
 
-Vecteur3D Vecteur3D::soustraction(Vecteur3D autre) const {
-  return addition(autre.oppose());
-}
+/* Methodes de la classe Vecteur3D: Definitions des methodes qui seront 
+ * utiles lors de calcul physique au cours de la modelisation: */
+ 
+ 
+Vecteur3D Vecteur3D::addition(Vecteur3D const& v) const {
+	Vecteur3D c(v.x+x, v.y+y, v.z+z);
+	return c;
+};
+
+
+Vecteur3D Vecteur3D::soustraction(Vecteur3D const& v) const {
+	return addition(v.oppose());
+};
+
 
 Vecteur3D Vecteur3D::oppose() const {
-  Vecteur3D resultat;
-  resultat.x(-x_);
-  resultat.y(-y_);
-  resultat.z(-z_);
-  return resultat;
-}
+	Vecteur3D c(-x, -y, -z);
+	return c;
+};
 
-Vecteur3D Vecteur3D::mult(double a) const {
-  Vecteur3D resultat;
-  resultat.x(a*x_);
-  resultat.y(a*y_);
-  resultat.z(a*z_);
-  return resultat;
-}
 
-double Vecteur3D::prod_scal(Vecteur3D autre) const {
-  return x_*autre.x() + y_*autre.y() + z_*autre.z();
-}
+Vecteur3D Vecteur3D::mult(double const& a) const {
+	Vecteur3D c(a*x, a*y, a*z);
+	return c;
+};
 
-Vecteur3D Vecteur3D::prod_vect(Vecteur3D v) const {
-  Vecteur3D resultat;
-  resultat.x(y_*v.z()-z_*v.y());
-  resultat.y(z_*v.x()-x_*v.z());
-  resultat.z(x_*v.y()-y_*v.x());
-  return resultat;
-}
+
+double Vecteur3D::prod_scal(Vecteur3D const& v) const {
+	return v.x*x+v.y*y+v.z*z;
+};
+
+
+Vecteur3D Vecteur3D::prod_vect(Vecteur3D const& v) const {
+	Vecteur3D c(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x);
+	return c;
+};
+
 
 double Vecteur3D::norme() const {
-  return sqrt(norme2());
-}
+	return sqrt(x*x+y*y+z*z);
+};
+
 
 double Vecteur3D::norme2() const {
-  return x_*x_ + y_*y_ + z_*z_;
-}
+	return x*x+y*y+z*z;
+};
+
 
 Vecteur3D Vecteur3D::unitaire() const {
-  Vecteur3D resultat;
-  double a(norme());
-  resultat.x(x_/a);
-  resultat.y(y_/a);
-  resultat.z(z_/a);
-  return resultat;
-}
+	Vecteur3D c(x/(sqrt(x*x+y*y+z*z)), y/(sqrt(x*x+y*y+z*z)), z/(sqrt(x*x+y*y+z*z)));
+	return c;
+};
 
-//setters
+//---------------------------------------------------------------
 
-void Vecteur3D::x(double x) {x_ = x;}
-void Vecteur3D::y(double y) {y_ = y;}
-void Vecteur3D::z(double z) {z_ = z;}
+/*Fonctions:*/
 
-//getters
-
-double Vecteur3D::x() const {return x_;}
-double Vecteur3D::y() const {return y_;}
-double Vecteur3D::z() const {return z_;}
-
-
-//fonctions
-
-double prod_mixte(Vecteur3D v1, Vecteur3D v2, Vecteur3D v3) {
-  return v1.prod_scal(v2.prod_vect(v3));
-}
+double prod_mixte(Vecteur3D const& u, Vecteur3D const& v, Vecteur3D const& w) {
+	return u.prod_scal(v.prod_vect(w));
+};
--- Vecteur3D.h
+++ Vecteur3D.h
@@ -1,33 +1,32 @@
 class Vecteur3D {
-private:
-  double x_, y_, z_;
-
-public:
-  void affiche() const;
-  bool compare(Vecteur3D autre) const;
-  void set(double x,double y,double z);
-
-  Vecteur3D addition(Vecteur3D autre) const;
-  Vecteur3D soustraction(Vecteur3D autre) const;
-  Vecteur3D oppose() const;
-  Vecteur3D mult(double a) const;
-  double prod_scal(Vecteur3D autre) const;
-  Vecteur3D prod_vect(Vecteur3D autre) const;
-
-  double norme() const;
-  double norme2() const;
-
-  Vecteur3D unitaire() const;
-
-//setters
-  void x(double x);
-  void y(double y);
-  void z(double z);
-
-//getters
-  double x() const;
-  double y() const;
-  double z() const;
+	private:
+		double x;  	// J'ai decide de ne pas choisir le type vector car
+		double y;	//je pense que la syntaxe d'initialisation sera trop
+		double z;	// longue et je visualiserai		
+					// personnellement moins bien les differents attributs 
+					// caracterisants  le vecteur 3D
+		
+	public:
+		Vecteur3D(double x=0.0, double y=0.0, double z=0.0) : x(x), y(y), z(z) {};
+		
+		/*Valuer par defaut a 0.0 car */
+		
+		void affiche() const;
+		bool compare(Vecteur3D) const;
+		
+		Vecteur3D addition(Vecteur3D const&) const;
+		Vecteur3D soustraction(Vecteur3D const&) const;
+		Vecteur3D oppose() const;
+		Vecteur3D mult(double const&) const;
+		double prod_scal(Vecteur3D const&) const;
+		Vecteur3D prod_vect(Vecteur3D const&) const;
+		double norme() const;
+		double norme2() const;
+		Vecteur3D unitaire() const;
+	
 };
 
-double prod_mixte(Vecteur3D v1, Vecteur3D v2, Vecteur3D v3);
+double prod_mixte(Vecteur3D const&, Vecteur3D const&, Vecteur3D const&);
+/* Nous avons decide de mettre le produit mixte comme une fonction qui 
+ * effectue le calcul et non pas comme une methode car cela allourdirait
+ * probablement la syntaxe*/
