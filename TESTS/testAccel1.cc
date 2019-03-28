2b728e1c4ecc72fe16132d2f70456abc54023e2a
HDHD2000 <47893466+HDHD2000@users.noreply.github.com>
2019-03-28 12:31:04 +0100

    Add files via upload

---
+++ TESTS/testAccel1.cc
@@ -0,0 +1,59 @@
+#include "Accelerateur.h"
+#include "Vue_Texte.h"
+using namespace std;
+
+//EXERCICE P9
+
+int main() {
+try {
+
+  Vue_Texte Vue(cout);
+	SupportADessin* p_Vue(new Vue_Texte(cout));
+
+	Accelerateur acc1(p_Vue);
+
+  //C'est plus logique de créer les éléments, particules en les intégrant à l'accélérateur, plutôt que de les créer et puis d'en mettre une copie dans l'accélérateur après. J'ai donc modifié la suite.
+
+  acc1.ajouter_el(new Dipole(Vecteur3D(1, 0, 0), Vecteur3D(0, -1, 0), 0.1, 1, 7, p_Vue));
+  acc1.ajouter_el(new SectionDroite(Vecteur3D(0, 5, 6), Vecteur3D (7, 0, 0), 0.4, p_Vue));
+	acc1.ajouter_par(new Particule(Vecteur3D(1.00984, -0.191837, 0), Vecteur3D(-210200, -2.64754e+08, 0), 2, 0.938272, e, p_Vue));
+	acc1.ajouter_par(new Particule(Vecteur3D(0.99016, -0.191837, 0), Vecteur3D(210200, -2.64754e+08, 0), 2, 0.938272, e, p_Vue));
+
+	cout << "Accelerateur 1 : " << endl;
+	Vue.dessine(acc1);
+	
+	/*Ancienne version du test:
+	cout << "Accelerateur 1 : " << endl;
+
+	cout << acc1 << endl;
+
+	cout << "Ajout d'un element a l'accelerateur 1 : " << endl;
+
+	p_Element el_(new ElementCourbe (el));
+	acc1.ajouter_el(el_);
+
+	cout << acc1 << endl;
+
+	cout << "Ajout d'une particule a l'accelerateur 1 : " << endl;
+
+	p_Particule p3_(new Particule (p3));
+	acc1.ajouter_par(p3_);
+
+	cout << acc1 << endl;
+
+	cout << "Supprimer tous les elements de l'accelerateur 1 : " << endl;
+
+	acc1.supprimer_el();
+
+	cout << acc1 << endl;
+
+	cout << "Supprimer toutes les particules de l'accelerateur 1 : " << endl;
+
+	acc1.supprimer_par();
+
+	cout << acc1 << endl;*/
+}
+catch (Erreur err) {cout << err.message;}
+
+	return 0;
+}
---
+++ TESTS/testParticule.cc
@@ -0,0 +1,74 @@
+#include <cmath>
+
+#include "Particule.h"
+#include "Vue_Texte.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+cout.setf(ios::scientific);
+cout << setprecision(10);
+
+SupportADessin* p_Vue(new Vue_Texte(cout));
+
+Particule p1(Vecteur3D (3.00984,-0.391837,0),Vecteur3D (-210200.0,-2.64754e+08,0), 2, 0.938272, e, p_Vue);
+Particule p2(Vecteur3D (2.99016,-0.391837,0),Vecteur3D (210200.0,-2.64754e+08,0), 2, 0.938272, e, p_Vue);
+
+/*cout << "P1 :" << endl << p1;
+cout << "P2 :" << endl << p2;*/
+
+cout << "P1 : "<< endl;
+p_Vue->dessine(p1);
+cout << "P2 : "<<endl;
+p_Vue->dessine(p2);
+
+Vecteur3D B(0,0,7); double dt(1e-11);
+cout << "Ajout d'une force magnétique B = " << B << "(dt=" << dt << ")" << endl;
+cout << " a P1:" << endl;
+p1.ajouter_f_magn(B,dt);
+cout << endl;
+cout << " a P2: " << endl;
+p2.ajouter_f_magn(B, dt);
+cout << endl;
+
+cout << "Les particules bougent une fois : " << endl;
+
+p1.bouger(dt);
+cout << "Nouveau P1 :" << endl;
+p_Vue->dessine(p1);
+cout << endl;
+
+p2.bouger(dt);
+cout << "Nouveau P2 : " << endl;
+p_Vue->dessine(p2);
+cout << endl;
+/*
+
+	cout << "Deux particules :" << endl << p1 << endl << " et " << p2 << endl;
+
+	Vecteur3D B(0.0, 0.0, 7);
+	const double dt (1e-11);
+
+	cout << "Ajout d'une force magnetique B = " << B << "( dt = " << dt << " ) : " << endl;
+	cout << "a p1 : " << endl;
+  p1.ajouteForceMagnetique(B,dt);
+  cout << p1 << endl;
+
+	cout << "a p2 : " << endl;
+  p2.ajouteForceMagnetique(B,dt);
+  cout << p2 << endl;
+
+	cout << "Les particules bougent une fois : ";
+
+	p1.bouger(dt);
+	p2.bouger(dt);
+
+	cout << p1 << endl;
+
+	cout << p2 <<endl;
+*/
+	return 0;
+
+}
